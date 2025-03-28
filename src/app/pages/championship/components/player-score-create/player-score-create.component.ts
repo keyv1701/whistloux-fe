248005f658bf0a968f22fb00b6a089b0161642be
Keyv <evrark.keyvin@gmail.com>
2025-03-28 17:45:30 +0100

    Add scrolling for create and edit week score popup

--- src/app/pages/championship/components/player-score-create/player-score-create.component.ts
+++ src/app/pages/championship/components/player-score-create/player-score-create.component.ts
@@ -1,30 +1,42 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PlayerScoreFormComponent } from '../player-score-form/player-score-form.component';
-import { PlayerFacade } from "../../../players/facades/player.facade";
+import { Component, EventEmitter, OnDestroy, OnInit, Output, Renderer2 } from '@angular/core';
+import { PlayerScoreFormComponent } from "../player-score-form/player-score-form.component";
 import { Observable } from "rxjs";
 import { PlayerLight } from "../../../../models/players/player-light.interface";
+import { PlayerFacade } from "../../../players/facades/player.facade";
 
 @Component({
+  standalone: true,
   selector: 'app-player-score-create',
   templateUrl: './player-score-create.component.html',
-  styleUrls: ['./player-score-create.component.scss'],
-  standalone: true,
-  imports: [CommonModule, PlayerScoreFormComponent]
+  imports: [
+    PlayerScoreFormComponent
+  ],
+  styleUrls: ['./player-score-create.component.css']
 })
-export class PlayerScoreCreateComponent {
+export class PlayerScoreCreateComponent implements OnInit, OnDestroy {
   @Output() close = new EventEmitter<void>();
-  @Output() saved = new EventEmitter<any>();
+  @Output() created = new EventEmitter<any>();
 
   public pseudos$: Observable<PlayerLight[]>;
 
-  public constructor(
+  constructor(
+    private renderer: Renderer2,
     private playerFacade: PlayerFacade
   ) {
     this.pseudos$ = this.playerFacade.loadPlayerPseudos();
   }
 
+  ngOnInit(): void {
+    // Désactiver le défilement du corps lorsque la popup est ouverte
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
+  }
+
+  ngOnDestroy(): void {
+    // Réactiver le défilement du corps lorsque la popup est fermée
+    this.renderer.setStyle(document.body, 'overflow', 'auto');
+  }
+
   onFormSubmit(formData: any): void {
-    this.saved.emit(formData);
+    this.created.emit(formData);
   }
 }
--- src/app/pages/championship/components/player-score-edit/player-score-edit.component.ts
+++ src/app/pages/championship/components/player-score-edit/player-score-edit.component.ts
@@ -1,5 +1,5 @@
 // player-score-edit.component.ts
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, Renderer2 } from '@angular/core';
 import { PlayerScoreFormComponent } from "../player-score-form/player-score-form.component";
 
 @Component({
@@ -16,27 +16,21 @@ export class PlayerScoreEditComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
   @Output() saved = new EventEmitter<any>();
 
-  constructor() {
+  constructor(private renderer: Renderer2) {
   }
+
 
   ngOnInit(): void {
     // Désactiver le défilement du corps lorsque la popup est ouverte
-    document.body.style.overflow = 'hidden';
+    this.renderer.setStyle(document.body, 'overflow', 'hidden');
   }
 
   ngOnDestroy(): void {
     // Réactiver le défilement du corps lorsque la popup est fermée
-    document.body.style.overflow = 'auto';
+    this.renderer.setStyle(document.body, 'overflow', 'auto');
   }
 
   onFormSubmit(formData: any): void {
     this.saved.emit(formData);
-  }
-
-  // Fermer la popup si l'utilisateur clique à l'extérieur (optionnel)
-  onBackdropClick(event: MouseEvent): void {
-    if ((event.target as HTMLElement).classList.contains('fixed')) {
-      this.close.emit();
-    }
   }
 }
