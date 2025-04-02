84a3c03012711b884ba15232cc7b46ae2c1ae910
Keyv <evrark.keyvin@gmail.com>
2025-04-02 12:38:58 +0200

    Add background color eligible players

--- src/app/models/championship/player-ranking.model.ts
+++ src/app/models/championship/player-ranking.model.ts
@@ -4,6 +4,7 @@ export interface PlayerRanking {
   rank: number;
   totalScore: number;
   roundsPlayed: number;
+  eligible: boolean;
   bestRoundScores: number[]; // 5 best rounds
   worstRoundScore: number; // Worst round included in total
 }
