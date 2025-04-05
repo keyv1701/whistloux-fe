4aefd08539aad8e905fff6840a68caa95582feef
Keyv <evrark.keyvin@gmail.com>
2025-04-05 20:42:38 +0200

    Adapt without https environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Remplacez '51.75.201.243' par l'IP ou le nom de domaine de votre serveur VPS
-  apiBaseUrl: 'https://51.75.201.243:8443/api'
+  apiBaseUrl: 'http://51.75.201.243:8080/api'
 };
