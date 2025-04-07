fb84b5742ce7dbaa7936bb6f0bbaa4bacdb3df51
Keyv <evrark.keyvin@gmail.com>
2025-04-07 08:46:43 +0200

    Add migration popup for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  showPopup = true;
+
+  closePopup() {
+    this.showPopup = false;
+  }
 
 }
