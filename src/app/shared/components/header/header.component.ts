50b984c0936c575f52aac5cbac9b58f399cb266a
Keyv <evrark.keyvin@gmail.com>
2025-04-11 08:57:13 +0200

    Add first config for fr-nl translations

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthFacade } from "../../security/auth/facades/auth.facade";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, TranslatePipe],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
   showMobileMenu = false;
+  currentLang: string;
+  showLanguageMenu = false;
 
-  constructor(public authFacade: AuthFacade) {
+  constructor(
+    public authFacade: AuthFacade,
+    private translateService: TranslateService
+  ) {
+    this.currentLang = this.translateService.currentLang || 'fr';
   }
 
   toggleMobileMenu() {
@@ -23,6 +30,27 @@ export class HeaderComponent {
 
   closeMobileMenu() {
     this.showMobileMenu = false;
+  }
+
+  changeLang(lang: string): void {
+    this.translateService.use(lang);
+    this.currentLang = lang;
+    this.showLanguageMenu = false;
+  }
+
+  toggleLanguageMenu(): void {
+    this.showLanguageMenu = !this.showLanguageMenu;
+  }
+
+  get availableLanguages(): { code: string, displayCode: string }[] {
+    return [
+      {code: 'fr', displayCode: 'FR'},
+      {code: 'nl', displayCode: 'NL'}
+    ];
+  }
+
+  get currentLanguage(): { code: string, displayCode: string } {
+    return this.availableLanguages.find(lang => lang.code === this.currentLang) || this.availableLanguages[0];
   }
 
   logout(): void {
