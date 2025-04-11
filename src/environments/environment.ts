f415d4bbe1c376261266e8cb7b78cd61851df8a4
Keyv <evrark.keyvin@gmail.com>
2025-04-11 08:57:13 +0200

    Add first config for fr-nl translations
    (cherry picked from commit 50b984c0936c575f52aac5cbac9b58f399cb266a)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,4 @@
-// src/environments/environment.ts
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api'
 };
-
-// // src/environments/environment.prod.ts
-// export const environment = {
-//   production: true,
-//   apiBaseUrl: '/api' // URL en production
-// };
